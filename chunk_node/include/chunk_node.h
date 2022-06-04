e705654b49239e73faaf292243b3bb5c0acea06f
corporal <corporal9736@gmail.com>
2022-06-05 07:00:42 +0800

    changes to chunk_node config file and data structure

--- chunk_node/include/chunk_node.h
+++ chunk_node/include/chunk_node.h
@@ -10,12 +10,18 @@ class chunk_node:public PublicSingleton<chunk_node>{
     public:
         chunk_node(const chunk_node&) = delete;
         chunk_node& operator=(const chunk_node&) = delete;
-        void init(std::string configPath);//chunk的信息，包含port，master的地址
+        void init(const std::string& configPath);//chunk的信息，包含port，master的地址
         int getPort();
         //然后是put和get方法
+        std::string get(const std::string& hash);
+        void put(const std::string& content, const std::string& hash);
+        void reportState() const;
     private:
         chunk_node();
-        int port;
+        address addr;
+        address master_addr;
+        std::string root;
+        std::uint64_t total_size; // size in byte, 21474836480 byte = 20GB
 };
 
 
